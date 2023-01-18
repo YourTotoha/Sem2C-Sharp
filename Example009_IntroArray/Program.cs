05c7d6fc66bdb8df050fb44f102c8d3093ddaad4
Vladimir <saintprize@gmail.com>
2023-01-18 16:49:46 +0300

    add new files from sem2

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max1(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 11, 221, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+
+int max = Max1(
+    Max1(array[0], array[1], array[2]),
+    Max1(array[3], array[4], array[5]),
+    Max1(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 232, 37, 12, 35, 66, 75, 84, 12 };
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
